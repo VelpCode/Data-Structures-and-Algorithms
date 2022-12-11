e889f15dbb8550c2f0f238a16c8d877f37e24cac
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2022-12-11 10:45:15 -0500

    Create Record.java

---
+++ Record.java
@@ -0,0 +1,26 @@
+//Aman Velpula, 251190328
+
+public class Record {
+
+    private String key;
+    private int score;
+    private int level;
+
+    public Record(String key, int score, int level) {
+        this.key = key;
+        this.score = score;
+        this.level = level;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
